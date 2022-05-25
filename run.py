f38a2ac9d6efff62441fc314f454349149b45e01
Stas <stasstas1807@gmail.com>
2022-05-25 12:35:06 +0300

    Mongo and country selection

--- run.py
+++ run.py
@@ -2,19 +2,19 @@ import logging
 from os import getenv
 
 from aiogram import Bot, Dispatcher, executor
-from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from aiogram.contrib.fsm_storage.mongo import MongoStorage
 from aiogram.types import Message
 
 from keyboards import ButtonText, Keyboards
+from lang import i18n
 from post import Post
 from tasks import Scheduler, PeriodicTask
-from lang import i18n
 
 if getenv('BOT_DEBUG'):
     logging.basicConfig(level=logging.DEBUG)
 
 bot = Bot(token=getenv('BOT_TOKEN'))
-mem = MemoryStorage()
+mem = MongoStorage(db_name='AvezorBot', uri=getenv('MONGODB_URL') or 'localhost')
 dp = Dispatcher(bot, storage=mem)
 
 dp.middleware.setup(i18n)
@@ -30,11 +30,28 @@ scheduler = Scheduler(
 
 @dp.message_handler(commands=['start'])
 async def h__start(msg: Message):
-    await msg.answer("Hi!\nI'm Avezor bot!", reply_markup=Keyboards.start)
+    await msg.answer("Hi!\nI'm Avezor bot!", reply_markup=Keyboards.country_selection)
+
+
+@dp.message_handler(text=ButtonText.country_georgia)
+async def h__any__country_georgia(msg: Message):
+    await i18n.set_locale(msg.from_user, 'ge')
+    await msg.answer('[что-то на грузинском (или и тут нужна мультиязычность?)]', reply_markup=Keyboards.start)
+
+
+@dp.message_handler(text=ButtonText.country_ukraine)
+async def h__any__country_ukraine(msg: Message):
+    await i18n.set_locale(msg.from_user, 'uk')
+    await msg.answer('Вы выбрали Украину.', reply_markup=Keyboards.start)
+
+
+@dp.message_handler(text=ButtonText.change_country)
+async def h__any__change_country(msg: Message):
+    await h__start(msg)
 
 
 @dp.message_handler(text=ButtonText.add_object)
-async def h__add__object(msg: Message):
+async def h__add_object(msg: Message):
     await msg.answer(_('buttonReply_addObject'), reply_markup=Keyboards.start)
 
 
@@ -57,11 +74,16 @@ async def h__lang(msg: Message, locale: str):
 
 
 @dp.message_handler(commands=['send'])
-async def h__send__post(msg: Message):
+async def h__send_post(msg: Message):
     posts = [Post('229500', 'com'), Post('231004', 'ge')]
     for post in posts:
         await bot.send_photo(msg.chat.id, post.get_photo_url(),
                              post.get_description(), reply_markup=post.get_buttons())
+
+
+@dp.message_handler()
+async def h__any__wrong(msg: Message):
+    await msg.answer(_("Something's wrong, I can feel it."))
 
 
 if __name__ == '__main__':
