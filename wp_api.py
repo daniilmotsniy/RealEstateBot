e529b2b9e2653b302d9603693cd2e5871f5148ad
Stas <stasstas1807@gmail.com>
2022-05-26 19:50:24 +0300

    Replace slugs with ids

--- wp_api.py
+++ wp_api.py
@@ -21,21 +21,21 @@ async def _query_wp(slug: str, country: str = None) -> Union[dict, list]:
             return await resp.json()
 
 
-async def get_regions() -> list[tuple[str, str]]:
-    return [(x['slug'], x['name']) for x in await _query_wp('property_county_state')]
+async def get_regions() -> list[tuple[int, str]]:
+    return [(x['id'], x['name']) for x in await _query_wp('property_county_state')]
 
 
-async def get_districts(region_name: str) -> list[tuple[str, str]]:
-    return [(x['slug'], x['name']) for x in await _query_wp('property_city') if x['stateparent'] == region_name]
+async def get_districts(region_name: str) -> list[tuple[int, str]]:
+    return [(x['id'], x['name']) for x in await _query_wp('property_city') if x['stateparent'] == region_name]
 
 
-async def get_action_types() -> list[tuple[str, str]]:
-    return [(x['slug'], x['name']) for x in await _query_wp('property_action_category')]
+async def get_action_types() -> list[tuple[int, str]]:
+    return [(x['id'], x['name']) for x in await _query_wp('property_action_category')]
 
 
-async def get_property_types() -> list[tuple[str, str]]:
-    return [(x['slug'], x['name']) for x in await _query_wp('property_category')]
+async def get_property_types() -> list[tuple[int, str]]:
+    return [(x['id'], x['name']) for x in await _query_wp('property_category')]
 
 
-async def get_room_counts() -> list[tuple[str, str]]:
-    return [(x['slug'], x['name']) for x in await _query_wp('property_count_rooms')]
+async def get_room_counts() -> list[tuple[int, str]]:
+    return [(x['id'], x['name']) for x in await _query_wp('property_count_rooms')]
